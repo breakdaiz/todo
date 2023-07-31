1e8389a7a0191f34286a798d78fa8fd31584234b
bernard rayoso <rayosobernard@gmail.com>
2023-08-01 03:53:25 +0800

    Implement Routing when login successfully

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,22 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   username = 'bernard';
   password = 'password';
-  errorMessage = '';
+  errorMessage = 'Invalid Credentials!';
   invalidLogin = false;
+
+  constructor(private router: Router) {
+    // ...
+  }
+
+  // Router
 
   submitHandler = () => {
     if (this.username === 'bernard' && this.password === 'password') {
-      this.invalidLogin = true;
-      this.errorMessage = 'Invalid Credentials!';
-    } else {
+      this.router.navigate(['welcome', this.username]);
       this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+      // this.errorMessage = 'Invalid Credentials!';
     }
   };
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent {
+  // ActivateRoute
+  name = '';
 
+  constructor(private router: ActivatedRoute) {
+    // ...
+  }
+
+  ngOnInit() {
+    this.name = this.router.snapshot.params['name'];
+  }
 }
