221fdb2289fa32cd83e1189b0147719a559f8f27
bernard rayoso <rayosobernard@gmail.com>
2023-08-01 04:36:13 +0800

    Create Todo Class, welcome component update router link

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {}
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,22 +16,11 @@ import { Component } from '@angular/core';
 })
 export class ListTodosComponent {
   todos = [
-    {
-      id: 1,
-      description: 'Learn to Dance',
-    },
-    {
-      id: 2,
-      description: 'Learn to Sing',
-    },
-    {
-      id: 3,
-      description: 'Learn to Cook',
-    },
-    {
-      id: 4,
-      description: 'Learn to Drive',
-    },
+    new Todo(0, 'Learn to Bike!', false, new Date()),
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Learn to Sing', false, new Date()),
+    new Todo(3, 'Learn to Cook', false, new Date()),
+    new Todo(4, 'Learn to Drive', false, new Date()),
   ];
   constructor() {}
 
